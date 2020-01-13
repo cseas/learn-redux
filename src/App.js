3a484d31639b5047e5c6f3eb25c6ccfd33286a4a
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2020-01-13 23:08:28 +0530

    Add project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>hello</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
