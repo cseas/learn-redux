394de39aa99fc9a229f80c7a37393b973681ad6d
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2020-01-13 23:08:54 +0530

    Add basic redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,44 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// create Store
+import { createStore } from "redux";
+// Example: let store = createStore(reducer);
+
+// Action is a function that returns an object
+const increment = () => {
+  return {
+    // name
+    type: "INCREMENT"
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
