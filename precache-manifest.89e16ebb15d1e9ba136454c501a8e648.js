4e9418615aec41096bbe37a0d32de3512a9482ad
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2020-01-14 22:50:14 +0530

    Updates

---
+++ precache-manifest.89e16ebb15d1e9ba136454c501a8e648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e850256aa5ce1235c29deec4049150ce",
+    "url": "/learn-redux/index.html"
+  },
+  {
+    "revision": "51aa64f50ec6fcd2be5d",
+    "url": "/learn-redux/static/css/main.a0b33bfa.chunk.css"
+  },
+  {
+    "revision": "ff038bcd9336fc835734",
+    "url": "/learn-redux/static/js/2.3ca277cb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learn-redux/static/js/2.3ca277cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51aa64f50ec6fcd2be5d",
+    "url": "/learn-redux/static/js/main.8c4b78a4.chunk.js"
+  },
+  {
+    "revision": "5f1b3705241184d44a5c",
+    "url": "/learn-redux/static/js/runtime-main.3731f4d9.js"
+  }
+]);
